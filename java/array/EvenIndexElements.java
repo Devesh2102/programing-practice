05da1b1a32c25f91eab395fb22fb99a05f976006
Devesh2102 <deveshkumar2102@gmail.com>
2025-04-18 22:25:24 +0000

    Even index Element

---
+++ java/array/EvenIndexElements.java
@@ -0,0 +1,8 @@
+class EvenIndexElements {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < arr.length; i += 2) {
+           System.out.println(arr[i]);
+        }
+    }
+}
