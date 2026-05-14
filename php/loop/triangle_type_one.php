91e1af80177cb6711d347beb66ab7301e34dcfb8
Devesh Kumar Prajapati <150000138+Devesh2102@users.noreply.github.com>
2026-05-15 02:50:16 +0530

    Delete php directory

--- php/loop/triangle_type_one.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Enter the number of rows: \n";
-$rowCount = trim(fgets(STDIN));
-
-for ($currentRow = 1; $currentRow <= $rowCount; $currentRow++) {
-    echo str_repeat("* ", $currentRow) . "\n";
-}
-?>
